8de4df5791e4d87c5811dec81b766fe30c7abf84
prlater <prlater@gmail.com>
2019-09-24 18:50:33 +0900

    Add FPS mode

---
+++ playerMoveScr/FirstPersonControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonControl : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public float upDownRange = 90;
+    public float jumpSpeed = 5;
+    public float downSpeed = 5;
+
+    private Vector3 speed;
+    private float forwardSpeed;
+    private float sideSpeed;
+
+    private float rotLeftRight;
+    private float rotUpDown;
+    private float verticalRotation = 0f;
+
+    private float verticalVelocity = 0f;
+
+    private CharacterController cc;
+
+    public Camera PCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Vector3 CVec;
+
+        CVec = new Vector3(cc.transform.position.x, cc.transform.position.y, transform.position.z - 2);
+
+        PCam.transform.Translate(CVec);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FPMove();
+        FPRotate();
+
+       
+
+    }
+
+    void FPMove()
+    {
+        forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+        sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        speed = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
+        speed = transform.rotation * speed;
+
+        cc.Move(speed * Time.deltaTime);
+       
+    }
+
+    void FPRotate()
+    {
+        rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        transform.Rotate(0f, rotLeftRight, 0f);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+
+       PCam.transform.rotation  = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+    }
+}
